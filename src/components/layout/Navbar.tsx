1efa5a8c8e7cb64b34db826ecab273b53ccf3ce2
jongwoo-lee <jongwoolee2014@u.northwestern.edu>
2020-02-15 13:12:55 +0900

    navbar logout menu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -11,10 +11,13 @@ import {
   AppBar,
   Grid,
   Typography,
-  IconButton
+  IconButton,
+  Menu,
+  MenuItem
 } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
 import { useAssociationValue } from "../../context/user";
+import Firebase from "../../helpers/Firebase";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -43,6 +46,22 @@ const useStyles = makeStyles((theme: Theme) =>
 export const Navbar = () => {
   const classes = useStyles();
   const ascData = useAssociationValue();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    setAnchorEl(null);
+    Firebase.fireLogout();
+  };
+
   return (
     <div>
       <AppBar position="sticky" className={classes.appBar}>
@@ -61,16 +80,36 @@ export const Navbar = () => {
           </Grid>
           {ascData && (
             <div className={classes.asc}>
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={ascData.email ?? undefined}
-                aria-haspopup="true"
-                color="primary"
-                style={{ margin: "0 0" }}
-              >
-                <AccountCircle fontSize="large" />
-              </IconButton>
+              <div>
+                <IconButton
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls="appbar-user"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="primary"
+                  style={{ margin: "0 0" }}
+                >
+                  <AccountCircle fontSize="large" />
+                </IconButton>
+                <Menu
+                  id="appbar-user"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleLogout}>로그아웃</MenuItem>
+                </Menu>
+              </div>
               <Typography style={{ margin: "auto auto" }} color="textPrimary">
                 {ascData.name}
               </Typography>
