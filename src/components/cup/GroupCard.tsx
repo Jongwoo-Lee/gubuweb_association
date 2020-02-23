baad4740f3227b4f5b92597989ea639c52a05009
jun-hak <keea9@naver.com>
2020-02-23 16:16:11 +0900

    card 추가

--- src/components/cup/GroupCard.tsx
+++ src/components/cup/GroupCard.tsx
@@ -1,43 +1,108 @@
 import React, { MouseEventHandler, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Card, Typography, CardActionArea, Dialog, DialogTitle, Button } from "@material-ui/core";
+import {
+  Card,
+  Typography,
+  CardActionArea,
+  Dialog,
+  DialogTitle,
+  Button,
+  IconButton
+} from "@material-ui/core";
 
 import { useHistory, useLocation } from "react-router-dom";
 import { SvgIconProps } from "@material-ui/core/SvgIcon";
 import { Team } from "../../helpers/Firebase/team";
 
 const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    width: 400,
+    minWidth: 150
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
 
-    root: {
-        display: "flex",
-        flexDirection: "row"
-    },
-    card: {
-        width: "100%",
-        minWidth: 150,
-        margin: "20px 20px"
-    },
+    justifyContent: "space-between",
+    width: 400,
+    minWidth: 150
+  },
+  items: {
+    display: "flex",
+    flexDirection: "row",
+    width: 400,
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  item: {
+    margin: "5px 20px"
+  }
 });
 
 // imgSrc 나 ImgIcon 둘중 하나만 꼭 넣어야 함
 export interface GroupCardProps {
+  numOfTeams: number;
+  group: number;
 }
 
-
 export const GroupCard: React.FC<GroupCardProps> = ({
+  numOfTeams,
+  group
 }: GroupCardProps) => {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <Card className={classes.card} variant="outlined">
+  const classes = useStyles();
+  const items: JSX.Element[] = Array<JSX.Element>();
+  for (let i = 0; i < numOfTeams; i++)
+    items.push(<GroupCardItem key={i} group={group} iter={i}></GroupCardItem>);
 
-                <Typography align="center" variant="body1" component="span">
-                    A조
+  return (
+    <div className={classes.root}>
+      <Card className={classes.card} variant="outlined">
+        <Typography
+          className={classes.item}
+          align="center"
+          variant="body1"
+          component="span"
+        >
+          {group}조
         </Typography>
-            </Card>
-            <Typography align="center" variant="body1" component="span">
-                이거 뭐더라
-        </Typography>
-        </div>
-    );
+        {items.length > 0 && items.map(item => item)}
+      </Card>
+    </div>
+  );
+};
+
+interface GroupCardItemProps {
+  group: number;
+  iter: number;
+}
+
+const GroupCardItem: React.FC<GroupCardItemProps> = ({
+  group,
+  iter
+}: GroupCardItemProps) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.items}>
+      <Typography
+        className={classes.item}
+        align="center"
+        variant="body1"
+        component="span"
+      >
+        {group}조 {iter}
+      </Typography>
+      <Typography
+        className={classes.item}
+        align="center"
+        variant="body1"
+        component="span"
+      >
+        <IconButton>+ 팀 추가</IconButton>
+      </Typography>
+    </div>
+  );
 };
