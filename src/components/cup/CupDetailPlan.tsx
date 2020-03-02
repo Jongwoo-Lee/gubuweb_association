5492c0c8b04a362e357dd682538ecd4955c041ba
jun-hak <keea9@naver.com>
2020-03-02 23:06:21 +0900

    일정 수정중

--- src/components/cup/CupDetailPlan.tsx
+++ src/components/cup/CupDetailPlan.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { ROUTENAMES } from "../../constants/routes";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { TitleGoBack } from "../common/TitleGoBack";
+import { Grid, Typography, Button } from "@material-ui/core";
+import { SaveMatchBtn } from "./match/SaveMatchInfo";
+import { RouteComponentProps } from "react-router-dom";
+import { MatchParams } from "./CupDetailTeam";
+import { useCupsInfo } from "../../context/cup/cup";
+import { CupInfo } from "../../helpers/Firebase/cup";
 
 export interface CupDetailPlanProps {}
 
@@ -11,21 +17,45 @@ const useStyles = makeStyles((theme: Theme) =>
       display: "flex",
       flexDirection: "column"
     },
-    cards: {
-      display: "flex",
-      marginTop: "40px",
-      flexWrap: "wrap"
+    title: {
+      width: "80%",
+      margin: "50px 0px 0px 0px"
     }
   })
 );
 
-export const CupDetailPlan: React.SFC<CupDetailPlanProps> = () => {
+export const CupDetailPlan: React.FC<RouteComponentProps<MatchParams>> = (
+  props: RouteComponentProps<MatchParams>
+) => {
   const classes = useStyles();
+  const cupID: string = props.match.params.cupID;
+  const cupsInfo = useCupsInfo();
+  let cupInfo: CupInfo | undefined;
+  if (cupsInfo !== undefined) cupInfo = cupsInfo[cupID];
+
+  const handleOnSave = async (
+    e: React.FormEvent<HTMLFormElement> | React.MouseEvent
+  ) => {
+    e.preventDefault();
+  };
 
   return (
     <div className={classes.root}>
-      <TitleGoBack title={ROUTENAMES.CUP_DETAIL_PLAN} />
-      <div className={classes.cards}>{ROUTENAMES.CUP_DETAIL_PLAN}</div>
+      <TitleGoBack title={cupInfo?.name ?? "No data"} />
+      <Grid
+        className={classes.title}
+        container
+        spacing={3}
+        justify="space-between"
+        alignItems="flex-start"
+      >
+        <Typography color="textPrimary" variant="h4">
+          {ROUTENAMES.CUP_DETAIL_TEAM}
+        </Typography>
+        <Button variant="contained" onClick={handleOnSave}>
+          저장
+        </Button>
+      </Grid>
     </div>
   );
 };
