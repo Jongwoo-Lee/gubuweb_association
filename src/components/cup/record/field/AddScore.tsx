e04e29049627dd817a6e58c6f28dbd2a29d1e51c
jun-hak <keea9@naver.com>
2020-03-19 09:39:19 +0900

    align

--- src/components/cup/record/field/AddScore.tsx
+++ src/components/cup/record/field/AddScore.tsx
@@ -1,79 +1,102 @@
 import React, { useState } from "react";
 import { Player } from "./Player";
-import { Box, makeStyles, Card, CardContent, Typography, ButtonBase, Grid } from "@material-ui/core";
+import {
+  Box,
+  makeStyles,
+  Card,
+  CardContent,
+  Typography,
+  ButtonBase,
+  Grid
+} from "@material-ui/core";
 import { RecordType } from "./RecordField";
-import { CurTime, useSelUsr, makeQuarterString } from "../../../../context/cup/cupRecord";
+import {
+  CurTime,
+  useSelUsr,
+  makeQuarterString
+} from "../../../../context/cup/cupRecord";
 import { TeamsPos } from "../../../../helpers/Firebase/game";
 import { convertTimeString } from "../../../../hooks/cups";
 import Trophy from "../../../../images/trophy_on.svg";
 
 export interface AddScoreProps {
-    time: CurTime;
-    teamPos: TeamsPos;
+  time: CurTime;
+  teamPos: TeamsPos;
 }
 
 const useStyles = makeStyles({
-    root: {
-        display: "flex",
-        flexDirection: "row",
-        margin: "20px",
-        padding: "15px",
-        justifyContent: "space-between",
-
-    },
-    col: {
-        display: "flex",
-        flexDirection: "column",
-    },
-    row: {
-        display: "flex",
-        flexDirection: "row",
-        alignContent: "center",
-        justifyContent: "center"
-
-    }
-
+  root: {
+    display: "flex",
+    flexDirection: "row",
+    margin: "20px",
+    padding: "15px",
+    justifyContent: "space-between"
+  },
+  col: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  row: {
+    display: "flex",
+    flexDirection: "row",
+    alignSelf: "center"
+  },
+  center: {
+    alignSelf: "center"
+  }
 });
 
 export const AddScore: React.FC<AddScoreProps> = ({
-    time, teamPos
+  time,
+  teamPos
 }: AddScoreProps) => {
-    const classes = useStyles();
-    const sel: number = useSelUsr();
-    const [scorers, setScorers] = useState<Array<string>>(Array<string>(2))
+  const classes = useStyles();
+  const sel: number = useSelUsr();
+  const [scorers, setScorers] = useState<Array<string>>(Array<string>(2));
 
-    const notEnter = () =>
-        (<div className={classes.row}>
-            <div><img width="30" src={Trophy} /></div>
-            <div>미입력</div>
-        </div>);
+  const notEnter = () => (
+    <div className={classes.row}>
+      <div className={classes.center}>
+        <img width="30" src={Trophy} alt={"팀원"} />
+      </div>
+      <Typography className={classes.center}>미입력</Typography>
+    </div>
+  );
 
-
-    return <Card variant="outlined" className={classes.root}>
-
-        <CardContent className={classes.col}>
-            <Typography align="center">득점</Typography>
-            <Typography>{convertTimeString(time.curTime)}</Typography>
-            <ButtonBase> <Typography>취소</Typography></ButtonBase>
-        </CardContent>
-        <CardContent className={classes.row}>
-            <Grid container direction="row" alignItems="center">
-                <Grid item>
-                    <Typography  >골:</Typography>
-                </Grid>
-                {scorers[0] !== undefined ? "입력" : notEnter()}
-
-            </Grid>
-        </CardContent>
-        <CardContent className={classes.col}>
-            <Typography align="center">득점</Typography>
-            <Typography>{convertTimeString(time.curTime)}</Typography>
-            <ButtonBase> <Typography>취소</Typography></ButtonBase>
-        </CardContent>
-        <CardContent className={classes.col}>
-            <Typography align="center">득점</Typography>
-            <Typography>{convertTimeString(time.curTime)}</Typography>
-            <ButtonBase> <Typography>취소</Typography></ButtonBase>
-        </CardContent>
-    </Card>;
+  return (
+    <Card variant="outlined" className={classes.root}>
+      <CardContent className={classes.col}>
+        <Typography align="center">득점</Typography>
+        <Typography>{convertTimeString(time.curTime)}</Typography>
+        <ButtonBase>
+          {" "}
+          <Typography>취소</Typography>
+        </ButtonBase>
+      </CardContent>
+      <CardContent className={classes.row}>
+        {/* <Grid container direction="row" alignItems="center"> */}
+        {/* <Grid item> */}
+        <Typography className={classes.center}>골:</Typography>
+        {/* </Grid> */}
+        {scorers[0] !== undefined ? "입력" : notEnter()}
+        {/* </Grid> */}
+      </CardContent>
+      <CardContent className={classes.col}>
+        <Typography align="center">득점</Typography>
+        <Typography>{convertTimeString(time.curTime)}</Typography>
+        <ButtonBase>
+          {" "}
+          <Typography>취소</Typography>
+        </ButtonBase>
+      </CardContent>
+      <CardContent className={classes.col}>
+        <Typography align="center">득점</Typography>
+        <Typography>{convertTimeString(time.curTime)}</Typography>
+        <ButtonBase>
+          {" "}
+          <Typography>취소</Typography>
+        </ButtonBase>
+      </CardContent>
+    </Card>
+  );
 };
